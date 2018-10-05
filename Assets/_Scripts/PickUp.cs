9488fde31455c52e008aafd6b80de1ec0eb94c1c
unknown <zantcho4@gmail.com>
2018-10-05 16:59:19 +0100

    add player and pick up

---
+++ Assets/_Scripts/PickUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+enum Type
+{
+    RecoveryItem,
+    Ressource,
+    QuestObject
+};
+
+public class PickUp {
+
+    int id;
+    string name;
+    string description;
+    Type type;
+    float exchangeValue;
+
+	public PickUp () {
+		
+	}
+
+    void selectType (Type newType)
+    {
+        type = newType;
+    }
+}
---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class Player {
+
+    float hp;
+    float maxHp;
+    float oxygenLevel;
+    bool isDead;
+
+    List<int> inventory;
+    List<int> quests;
+    float amountOfMoney;   
+
+	public Player() {
+        hp = 10;
+        maxHp = 10;
+        oxygenLevel = 1.0;
+        isDead = false;
+	}
+
+    void takeDamages(float dmg)
+    {
+        hp -= dmg;
+        if (hp <= 0)
+            isDead = true;
+    }
+
+    void recoverHp(float heal)
+    {
+        hp += heal;
+        if (hp > maxHp)
+            hp = maxHp;
+    }
+
+    void addItem(int itemId)
+    {
+        inventory.Add(itemId);
+    }
+
+    void removeItem(int itemId)
+    {
+        inventory.Remove(itemId);
+    }
+
+    void addQuest(int questId)
+    {
+        quests.Add(questId);
+    }
+
+    void removeQuest(int questId)
+    {
+        quests.Remove(questId);
+    }
+
+    void earnMoney(float money)
+    {
+        amountOfMoney += money;
+    }
+
+    void looseMoney(float money)
+    {
+        amountOfMoney -= money;
+    }
+
+}
