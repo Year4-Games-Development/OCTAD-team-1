2c4a2bee83834725abcb5e2ce3729e0744c7972f
zantcho <zantcho4@gmail.com>
2018-10-25 14:07:37 +0100

    adding regular item, consumable item and quest class

---
+++ Assets/_Scripts/RegularItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegularItem : Item {
+    private int _id;
+    private string _name;
+    private string _description;
+
+    public RegularItem(int id, string name, string description)
+    {
+        _id = id;
+        _name = name;
+        _description = description;
+    }
+
+    public int id
+    {
+        get { return _id;}
+        set { _id = value;}
+    }
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+    public string Description
+    {
+        get { return _description; }
+        set { _description = value; }
+    }
+
+    public string use()
+    {
+        return Description;
+    }
+
+
+
+}
